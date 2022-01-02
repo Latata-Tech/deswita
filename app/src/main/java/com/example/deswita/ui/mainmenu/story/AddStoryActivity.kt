9588013bed89e960283ffbf397807009e544410c
afrizafarhan <farhanafriza24@gmail.com>
2022-01-02 15:00:32 +0700

    done to add screen add story

---
+++ app/src/main/java/com/example/deswita/ui/mainmenu/story/AddStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.deswita.ui.mainmenu.story
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deswita.R
+
+class AddStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
