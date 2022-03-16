06fe6af9fe0fb0817752e0cd37dfd736819a6935
afrizafarhan <farhanafriza24@gmail.com>
2022-03-17 00:07:21 +0700

    feat: add threads to select image

--- app/src/main/java/com/example/deswita/ui/mainmenu/event/AddEventActivity.kt
+++ app/src/main/java/com/example/deswita/ui/mainmenu/event/AddEventActivity.kt
@@ -1,22 +1,32 @@
 package com.example.deswita.ui.mainmenu.event
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import com.example.deswita.R
 import com.example.deswita.databinding.ActivityAddEventBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class AddEventActivity : AppCompatActivity() {
 
     private var _binding: ActivityAddEventBinding? = null
     private val binding get() = _binding
-
+    private val requestCodeEvent = 200
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_event)
         _binding = ActivityAddEventBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        binding?.btnSelectImage?.setOnClickListener {
+            selectImage()
+        }
         binding?.btnBack?.setOnClickListener {
             onBackPressed()
         }
@@ -24,6 +34,25 @@ class AddEventActivity : AppCompatActivity() {
         binding?.btnAddEvent?.setOnClickListener {
             Toast.makeText(this, "save event!", Toast.LENGTH_SHORT).show()
         }
+    }
 
+    fun selectImage()
+    {
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(intent, requestCodeEvent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == requestCodeEvent){
+            Thread(Runnable {
+                imageUri = data?.data
+                Timer().schedule(900){
+                    binding?.ivAddEvent?.post {
+                        binding?.ivAddEvent?.setImageURI(imageUri)
+                    }
+                }
+            }).start()
+        }
     }
 }
