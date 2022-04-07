fa41b30d5bf79e85c44f8db4d68c5e5249b9234c
farhan <farhanafriza24@gmail.com>
2022-04-07 13:04:01 +0700

    fix: set content intent when click notification

--- app/src/main/java/com/example/deswita/service/NotificationScheduleReceiver.kt
+++ app/src/main/java/com/example/deswita/service/NotificationScheduleReceiver.kt
@@ -2,12 +2,14 @@ package com.example.deswita.service
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.deswita.R
+import com.example.deswita.ui.MainActivity
 import java.util.*
 
 class NotificationScheduleReceiver : BroadcastReceiver() {
@@ -19,11 +21,14 @@ class NotificationScheduleReceiver : BroadcastReceiver() {
                 val channel_id = "channel_notification_1"
                 val importance = NotificationManager.IMPORTANCE_HIGH
                 val channel = NotificationChannel(channel_id, "schedule_notification", importance)
+                val notifyIntent = Intent(context, MainActivity::class.java)
+                val notifyPendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
                 val notificationBuilder = NotificationCompat.Builder(context!!, channel_id)
                     .setSmallIcon(R.drawable.ic_deswita_icon)
                     .setContentText("Mau kemana liburan minggu ini?")
                     .setContentTitle("Yay!! hari libur tiba")
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(notifyPendingIntent)
                 val notificationManager : NotificationManager  = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
                 for(s in notificationManager.notificationChannels){
