a595ebcddc3b732c30827d34cf2b0746926a1b62
afrizafarhan <farhanafriza24@gmail.com>
2022-04-15 15:58:32 +0700

    feat: add taskstackbuild

--- app/src/main/java/com/example/deswita/ui/mainmenu/event/AddEventActivity.kt
+++ app/src/main/java/com/example/deswita/ui/mainmenu/event/AddEventActivity.kt
@@ -15,6 +15,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 import com.example.deswita.R
 import com.example.deswita.databinding.ActivityAddEventBinding
 import com.example.deswita.models.Event
@@ -65,9 +66,12 @@ class AddEventActivity : AppCompatActivity() {
                 location.isEmpty() -> binding?.edtLocationAddEvent?.error = "required"
                 title.isEmpty() -> binding?.edtTitleAddEvent?.error = "required"
                 description.isEmpty() -> binding?.edtDescriptionAddEvent?.error = "required"
-                imageUri == null -> Toast.makeText(this,"image required",Toast.LENGTH_SHORT).show()
+                imageUri == null -> Toast.makeText(this, "image required", Toast.LENGTH_SHORT)
+                    .show()
                 else -> {
-                    val event = Event(121,imageUri.toString(),date,title,location,description)
+                    val idEvent = (1..9999).random()
+                    val event =
+                        Event(idEvent, imageUri.toString(), date, title, location, description)
                     sendNotification(event)
                 }
             }
@@ -77,21 +81,21 @@ class AddEventActivity : AppCompatActivity() {
             val calendarFragment = CalendarFragment()
 
             val bundle = Bundle()
-            bundle.putString(EventFragment.EXTRA_DATE,date)
+            bundle.putString(EventFragment.EXTRA_DATE, date)
             calendarFragment.arguments = bundle
 
-            calendarFragment.show(supportFragmentManager,CalendarFragment::class.java.simpleName)
-            calendarFragment.setOnClickItemCallback(object: CalendarFragment.OnClickItemCallback{
+            calendarFragment.show(supportFragmentManager, CalendarFragment::class.java.simpleName)
+            calendarFragment.setOnClickItemCallback(object : CalendarFragment.OnClickItemCallback {
                 override fun onClick(dt: Date) {
                     setDate(dt)
                 }
             })
         }
 
-        if(savedInstanceState != null) {
+        if (savedInstanceState != null) {
             date = savedInstanceState.getString(EXTRA_DATE).toString()
             binding?.tvDateEvent?.text = date
-        }else {
+        } else {
             setDate(Date())
         }
     }
@@ -104,30 +108,34 @@ class AddEventActivity : AppCompatActivity() {
     }
 
     private fun createNotificationChannel() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "Event"
             val descriptionText = "New event appeared"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
                 description = descriptionText
             }
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
     }
 
     private fun sendNotification(event: Event) {
 
-        val intent = Intent(this,EventActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            putExtra(EventActivity.EXTRA_EVENT,event)
-        }
+        val intent = Intent(this, EventActivity::class.java)
+        intent.putExtra(EventActivity.EXTRA_EVENT, event)
 
-        val pendingIntent = PendingIntent.getActivity(this,0,intent,0)
-        val bitmapLargeIcon = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.ic_deswita_icon)
-        val bitmapImage = MediaStore.Images.Media.getBitmap(contentResolver,imageUri)
+        val pendingIntent = TaskStackBuilder.create(this)
+            .addParentStack(EventActivity::class.java)
+            .addNextIntent(intent)
+            .getPendingIntent(110, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+        val bitmapLargeIcon =
+            BitmapFactory.decodeResource(applicationContext.resources, R.drawable.ic_deswita_icon)
+        val bitmapImage = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_deswita_icon)
             .setContentTitle(event.name)
             .setContentText(event.description)
@@ -135,24 +143,24 @@ class AddEventActivity : AppCompatActivity() {
             .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmapImage))
             .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
 
         with(NotificationManagerCompat.from(this)) {
-            notify(notificationId,builder.build())
+            notify(notificationId, builder.build())
         }
     }
 
-    fun selectImage()
-    {
+    fun selectImage() {
         var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         startActivityForResult(intent, requestCodeEvent)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK && requestCode == requestCodeEvent){
+        if (resultCode == RESULT_OK && requestCode == requestCodeEvent) {
             Thread(Runnable {
                 imageUri = data?.data
-                Timer().schedule(900){
+                Timer().schedule(900) {
                     binding?.ivAddEvent?.post {
                         binding?.ivAddEvent?.setImageURI(imageUri)
                     }
@@ -162,7 +170,7 @@ class AddEventActivity : AppCompatActivity() {
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(EXTRA_DATE,date)
+        outState.putString(EXTRA_DATE, date)
         super.onSaveInstanceState(outState)
     }
 }
